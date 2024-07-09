df770196701a601b6fe0d5f1b41457a7036b4e33
alfredohuels <alfredohuels@hotmail.com>
2024-07-09 15:08:28 -0300

    modificacion para que funcionen las variables externas

--- API_CRUD/data/db.js
+++ API_CRUD/data/db.js
@@ -1,17 +1,10 @@
-const {Sequelize} = require ("sequelize")
+const { Sequelize } = require('sequelize');
 
-/*  nombre de la base de datos -  user - contraseña - {donde esta alojada?,lenguaje, puerto} */
+const sequelize = new Sequelize(process.env.MYSQL_ADDON_DB, process.env.MYSQL_ADDON_USER, process.env.MYSQL_ADDON_PASSWORD, {
+  host: process.env.MYSQL_ADDON_HOST,
+  dialect: 'mysql',
+  port: process.env.MYSQL_ADDON_PORT,
+  logging: false, // Puedes deshabilitar el logging si no lo necesitas
+});
 
-const db = new Sequelize ("posteos24255","root","",{
-    hostDB: process.env.MYSQL_ADDON_HOST,
-    dialect: "mysql",
-    portDB: process.env.MYSQL_ADDON_PORT
-})
-
-// const db = new Sequelize (process.env.MYSQL_ADDON_DB,process.env.MYSQL_ADDON_USER,process.env.MYSQL_ADDON_PASSWORD,{
-//     host: process.env.MYSQL_ADDON_HOST,
-//     dialect: "mysql",
-//     port: process.env.MYSQL_ADDON_PORT,
-// })
-
-module.exports= db
+module.exports = sequelize;
--- API_CRUD/index.js
+++ API_CRUD/index.js
@@ -1,36 +1,38 @@
-// import 'dotenv/config';
-const dotenv = require ("dotenv/config")
-const express = require ("express")
-const cors = require ("cors") 
-const postRouter = require ("./routes/postRouter.js")
-const app = express()
-const portServidor = 3030
-const portBD = process.env.PORT || 3030
-const db = require ("./data/db.js")
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const postRouter = require('./routes/postRouter.js');
+const app = express();
+const portServidor = 3030;
+const db = require('./data/db.js');
 
-app.use(cors()) // habilito el intercambio de información
-app.use(express.json()) // analiza los request
+console.log('Host:', process.env.MYSQL_ADDON_HOST);
+console.log('Database:', process.env.MYSQL_ADDON_DB);
+console.log('User:', process.env.MYSQL_ADDON_USER);
+console.log('Password:', process.env.MYSQL_ADDON_PASSWORD);
+console.log('Puerto:', process.env.MYSQL_ADDON_PORT);
 
-app.get ("/",(req,res)=>{
-res.send ("estas en el home")
-})  // sin modularizar
 
-app.use ("/posteos",postRouter) // modularizado
+app.use(cors()); // habilito el intercambio de información
+app.use(express.json()); // analiza los request
 
-// conexion a la base de datos
+app.get('/', (req, res) => {
+    res.send('Estas en el home');
+});  // sin modularizar
 
-const conexionDB = async ()=>{
+app.use('/posteos', postRouter); // modularizado
+
+// Conexion a la base de datos
+const conexionDB = async () => {
     try {
-        await db.authenticate()
-        console.log(`Conectado Ok a la Base de datos`);
+        await db.authenticate();
+        console.log('Conectado Ok a la Base de datos');
     } catch (error) {
         console.log(`Hay un error y es el siguiente : ${error}`);
     }
-}
+};
 
-
-app.listen (portServidor,()=>{
-    conexionDB()
+app.listen(portServidor, () => {
+    conexionDB();
     console.log(`Servidor Ok en el puerto ${portServidor}`);
-})
-
+});
